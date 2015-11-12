b5025d84b95ca565b181423376f481270bcd94c0
Wu Jun <499675898@qq.com>
2015-11-12 15:05:31 +0800

    xiaoxiugai

--- app/controllers/ctsinits_controller.rb
+++ app/controllers/ctsinits_controller.rb
@@ -6,7 +6,7 @@ class CtsinitsController < ApplicationController
     end
 
     if Cfg.count == 0
-      Cfg.create(sale_tel:"0877-123456789",email:"xxxxxxxxxxx@xxxx.xxx",company_name:"xxxxxxxxxxxxxxxx",address:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",content:"xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
+      Cfg.create(sale_tel:"0877-123456789",email:"xxxxxxxxxxx@xxxx.xxx",company_name:"云南泽隆工贸有限公司",address:"xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx",content:"xxxxxxxxxxxxxxxxxxxxxxxxxxxx")
     end
 
   end
