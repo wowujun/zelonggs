63cd308b64eaeb5592448346ef4ada26e8fb3d56
Wu Jun <499675898@qq.com>
2015-11-12 11:57:13 +0800

    zxc

--- app/controllers/sides_controller.rb
+++ app/controllers/sides_controller.rb
@@ -9,9 +9,7 @@ class SidesController < ApplicationController
       @cla = Sidecla.find_by_keywords("news")
     @sidecontents = @cla.sidecontents
       @searchs = @sidecontents
-    else
-      @cla = Sidecla.find_by_keywords("products")
-      @sidecontents = @cla.sidecontents
+
       end
 
   end
